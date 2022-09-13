dd9e648b7fcff058816cd0d24c8fc808dec91b11
LAPTOP-3ACO8IHI\janze <janzen_valeria@outlook.com>
2022-09-12 22:00:29 -0300

    Contexto Léxico

---
+++ web moderno/modulo 1/funçao/contextoLéxico.js
@@ -0,0 +1,17 @@
+const valor = 'Global'
+
+function minhaFuncao() {
+    console.log(valor)
+}
+
+function exec() {
+    const valor = 'Local'
+    minhaFuncao()
+}
+
+exec()
+
+// Quando uma função é declarada dentro de javascript ela 
+//nao tem apenas o seu conteudo interno, o seu nome, os paramentros que ela recebe
+//e aquilo que ela retorna, ela tambem tem conciência no local que ela foi definida
+// A função carrega consigo o local em que foi definida
